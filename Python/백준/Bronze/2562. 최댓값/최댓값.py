668d1982d1c84fa3437b06a5c93b9d119690d50f
김태욱 <xodnr34@naver.com>
2024-08-12 23:45:10 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+
+max = (max(a,b,c,d,e,f,g,h,i))
+print(max)
+list = [a,b,c,d,e,f,g,h,i]
+print(list.index(max)+1)
