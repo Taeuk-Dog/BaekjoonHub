d041e1c14d31843e5a02aebef66171526b06fe6f
김태욱 <xodnr34@naver.com>
2024-08-16 19:58:10 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,15 +1,15 @@
-SELECT
-ANIMAL_ID,
-NAME,
-CASE WHEN
-    SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
-    WHEN
-    SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
-    ELSE
-    'X'
-END AS 
-중성화
+select
+    ANIMAL_ID, 
+    NAME,
+    CASE WHEN
+        SEX_UPON_INTAKE LIKE 'Neutered%' then 'O'
+        when
+        SEX_UPON_INTAKE like 'Spayed%' then 'O'
+        else
+            'X'
+            end as
+    중성화
 FROM
-ANIMAL_INS
+    ANIMAL_INS
 ORDER BY
-ANIMAL_ID
+    ANIMAL_ID
