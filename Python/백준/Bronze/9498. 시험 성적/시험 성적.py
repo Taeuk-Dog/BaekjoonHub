22a53acd95513f6d664b6e3260864e9707be8fae
김태욱 <xodnr34@naver.com>
2024-08-19 22:59:38 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+user_input = int(input())
+if 90 <= user_input <= 100:
+    print("A")
+elif 80<= user_input <=89:
+    print('B')
+elif 70 <=user_input<=79:
+    print('C')
+elif 60<= user_input <= 69:
+    print('D')
+else:
+    print('F')
